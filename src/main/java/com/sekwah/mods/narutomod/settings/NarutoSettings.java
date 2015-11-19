06a77146d2080264a20de40fa80bb02bdc40092d
Alastair <alastair.hawkes@gmail.com>
2015-11-19 22:42:20 +0000

    Changed things

--- src/main/java/com/sekwah/mods/narutomod/settings/NarutoSettings.java
+++ src/main/java/com/sekwah/mods/narutomod/settings/NarutoSettings.java
@@ -24,7 +24,7 @@ public class NarutoSettings {
 
     public static int jutsuDelay = 10; // Delay time in ticks, from when the last button is presset till the jutsu casts
 
-    public static int usageReportMod = 0;
+    public static int usageReportMode = 0;
 
     public static void changeSetting() {
     }
@@ -141,9 +141,9 @@ public class NarutoSettings {
             jutsuDelay = 0;
         }
 
-        Property configUsageReportMod = config.get(Configuration.CATEGORY_GENERAL, "usageReportMod", 0);
-        usageReportMod = configUsageReportMod.getInt(0);
-        configUsageReportMod.comment = "This sets the usage report mode 0 = Enabled, 1 = No data sent but says its " +
+        Property configUsageReportMode = config.get(Configuration.CATEGORY_GENERAL, "usageReportMode", 0);
+        usageReportMode = configUsageReportMode.getInt(0);
+        configUsageReportMode.comment = "This sets the usage report mode 0 = Enabled, 1 = No data sent but says its " +
                 "online, 2 = Disabled (please leave this on 1 at least just for me :3 the data is anonymous and it " +
                 "lets me see how many people are playing. If you dont like data being sent about your pc such as " +
                 "operaing system and cores then please set it to 1 so we have a usage count)";
