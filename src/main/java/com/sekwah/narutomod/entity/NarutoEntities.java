b7effb99253a5fc3fa9318b6bf1c6c815093f755
Sekwah <sekwah41@gmail.com>
2020-11-12 02:42:10 +0000

    Fixed client kunai bug

--- src/main/java/com/sekwah/narutomod/entity/NarutoEntities.java
+++ src/main/java/com/sekwah/narutomod/entity/NarutoEntities.java
@@ -17,7 +17,7 @@ public class NarutoEntities {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, MOD_ID);
 
     public static final RegistryObject<EntityType<KunaiEntity>> KUNAI =register("kunai",
-            EntityType.Builder.<KunaiEntity>create(KunaiEntity::new, EntityClassification.MISC).size(0.5F, 0.5F).trackingRange(4).func_233608_b_(20));
+            EntityType.Builder.<KunaiEntity>create(KunaiEntity::new, EntityClassification.MISC).size(0.5F, 0.5F).trackingRange(8));
 
     private static <T extends Entity> RegistryObject<EntityType<T>> register(String key, EntityType.Builder<T> builder) {
         return ENTITIES.register(key, () -> builder.build(key));
