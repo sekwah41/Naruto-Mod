c4541adf1b9ee978ea752ad54d5b090ba3672107
Sekwah <sekwah41@gmail.com>
2021-05-19 20:18:01 +0100

    ci: Add automated changelogs and moved location

--- .versionrc.js
+++ .versionrc.js
@@ -14,5 +14,13 @@ const tracker = {
 
 module.exports = {
   bumpFiles: [tracker],
-  packageFiles: [tracker]
+  packageFiles: [tracker],
+  // In case you need to force a version change (mostly due to change of scope of the update e.g. major now instead of patch)
+  //releaseAs: '0.16.0',
+  header:"# Changelog\n" +
+      "\n" +
+      "All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.\n" +
+      "\n" +
+      "For the release changelogs see [CHANGELOG.md](CHANGELOG.md)  \n" +
+      "For the snapshot changelogs see [SNAPSHOT_CHANGELOG.md](SNAPSHOT_CHANGELOG.md)\n",
 }
