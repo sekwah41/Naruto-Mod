ee3de166f25b21c73f8757a4c4e06f02c861eafd
Sekwah <contact@sekwah.com>
2021-06-16 22:07:01 +0100

    feat: Add item recipies

---
+++ .github/PULL_REQUEST_TEMPLATE/pr_changelog_release_notes.md
@@ -0,0 +1,5 @@
+# What Changed
+Whatever changed here
+
+## Release Notes
+If anything should be commented in the changelog release notes.
--- src/main/java/com/sekwah/narutomod/datagen/RecipeGen.java
+++ src/main/java/com/sekwah/narutomod/datagen/RecipeGen.java
@@ -1,10 +1,8 @@
 package com.sekwah.narutomod.datagen;
 
+import com.sekwah.narutomod.block.NarutoBlocks;
 import com.sekwah.narutomod.item.NarutoItems;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.IFinishedRecipe;
-import net.minecraft.data.RecipeProvider;
-import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraft.data.*;
 import net.minecraft.item.Items;
 
 import java.util.function.Consumer;
@@ -24,5 +22,43 @@ public class RecipeGen extends RecipeProvider {
                 .unlockedBy("has_rose_red", has(Items.RED_DYE))
                 .unlockedBy("has_iron_ingot", has(Items.WHITE_TERRACOTTA))
                 .save(recipeConsumer);
+
+        ShapedRecipeBuilder.shaped(NarutoItems.KUNAI.get(), 8).define('I', Items.IRON_INGOT).define('S', Items.STICK)
+                .pattern(" I")
+                .pattern("S ")
+                .unlockedBy("has_iron_ingot", has(Items.IRON_INGOT))
+                .save(recipeConsumer);
+
+        ShapedRecipeBuilder.shaped(NarutoItems.SHURIKEN.get(), 16).define('I', Items.IRON_INGOT).define('F', Items.FLINT)
+                .pattern(" F ")
+                .pattern("FIF")
+                .pattern(" F ")
+                .unlockedBy("has_iron_ingot", has(Items.IRON_INGOT))
+                .unlockedBy("has_flint", has(Items.FLINT))
+                .save(recipeConsumer);
+
+        ShapedRecipeBuilder.shaped(NarutoItems.SENBON.get(), 16).define('N', Items.IRON_NUGGET)
+                .pattern("  N")
+                .pattern(" N ")
+                .pattern("N  ")
+                .unlockedBy("has_iron_nugget", has(Items.IRON_NUGGET))
+                .save(recipeConsumer);
+
+        ShapelessRecipeBuilder.shapeless(NarutoItems.EXPLOSIVE_KUNAI.get())
+                .requires(NarutoItems.KUNAI.get())
+                .requires(NarutoBlocks.ITEM_PAPER_BOMB.get())
+                .unlockedBy("has_paper_bomb", has(NarutoBlocks.ITEM_PAPER_BOMB.get()))
+                .save(recipeConsumer);
+
+        ShapedRecipeBuilder.shaped(NarutoBlocks.ITEM_PAPER_BOMB.get())
+                .define('G', Items.GUNPOWDER)
+                .define('P', Items.PAPER)
+                .pattern("GGG")
+                .pattern("PPP")
+                .pattern("GGG")
+                .unlockedBy("has_gunpowder", has(Items.GUNPOWDER))
+                .save(recipeConsumer);
+
+
     }
 }
