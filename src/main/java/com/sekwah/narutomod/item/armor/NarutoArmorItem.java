d5ccc3737bc12b007ce6f260b72421132ea9f3d7
Sekwah <contact@sekwah.com>
2021-06-30 23:31:59 +0100

    feat: Anbu masks now hide usernames

--- src/main/java/com/sekwah/narutomod/item/armor/NarutoArmorItem.java
+++ src/main/java/com/sekwah/narutomod/item/armor/NarutoArmorItem.java
@@ -1,6 +1,7 @@
 package com.sekwah.narutomod.item.armor;
 
 import com.sekwah.narutomod.NarutoMod;
+import com.sekwah.narutomod.item.interfaces.IShouldHideNameplate;
 import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
@@ -11,10 +12,11 @@ import net.minecraft.item.ItemStack;
 
 import javax.annotation.Nullable;
 
-public class NarutoArmorItem extends ArmorItem {
+public class NarutoArmorItem extends ArmorItem implements IShouldHideNameplate {
 
     private BipedModel armorModel = null;
     private String armorTexture = null;
+    private boolean forceHideName = false;
 
     public NarutoArmorItem(IArmorMaterial p_i48534_1_, EquipmentSlotType p_i48534_2_, Properties p_i48534_3_) {
         super(p_i48534_1_, p_i48534_2_, p_i48534_3_);
@@ -35,8 +37,18 @@ public class NarutoArmorItem extends ArmorItem {
         return this;
     }
 
+    public NarutoArmorItem setShouldHideNameplate(boolean shouldHide) {
+        this.forceHideName = shouldHide;
+        return this;
+    }
+
     public NarutoArmorItem setArmorTexture(String armorTexture) {
         this.armorTexture = NarutoMod.MOD_ID + ":" + armorTexture;
         return this;
+    }
+
+    @Override
+    public boolean shouldHideNameplate(Entity entity) {
+        return this.forceHideName;
     }
 }
