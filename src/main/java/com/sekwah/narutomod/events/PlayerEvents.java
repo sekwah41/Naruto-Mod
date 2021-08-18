0881d3863d9ddab34ba5b4941d055d312223487a
Sekwah <contact@sekwah.com>
2021-08-18 22:31:31 +0100

    refactor: Reorganise event code

---
+++ src/main/java/com/sekwah/narutomod/events/PlayerEvents.java
@@ -0,0 +1,33 @@
+package com.sekwah.narutomod.events;
+
+import com.sekwah.narutomod.NarutoMod;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = NarutoMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class PlayerEvents {
+
+    @SubscribeEvent
+    public void onEntityUpdate(LivingEvent.LivingUpdateEvent event) {
+        // Do chakra updates and other stuff here unless handled in capabilities
+    }
+
+// Handle if they have some agility perk or leaps.
+//
+//    @SubscribeEvent
+//    public void livingFall(LivingFallEvent event) {
+//        if (event.getEntity() instanceof Player player){
+//          //NarutoMod.logger.info(event.distance);
+//            if(event.distance < 9){
+//                event.distance *= 0.3f;
+//            }
+//            if(event.distance > 3) {
+//                event.distance -= 5f;
+//                event.distance *= 0.6f;
+//            }
+//        }
+//    }
+
+}
