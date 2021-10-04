7eb044b6bf5209e94af01f8adbc2efbf48e3b227
Sekwah <contact@sekwah.com>
2021-10-04 19:17:38 +0100

    fix: Paper bomb water blocks

--- src/main/java/com/sekwah/narutomod/block/NarutoMaterial.java
+++ src/main/java/com/sekwah/narutomod/block/NarutoMaterial.java
@@ -1,4 +1,13 @@
 package com.sekwah.narutomod.block;
 
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
+
 public class NarutoMaterial {
+
+    /**
+     * Can be broken by pistons
+     */
+    public static final Material UNSTABLE_EXPLOSIVE = (new Material.Builder(MaterialColor.FIRE)).noCollider().flammable().notSolidBlocking().nonSolid().destroyOnPush().build();
+
 }
