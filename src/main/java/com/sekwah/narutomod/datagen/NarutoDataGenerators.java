140f4732820a3ccbb46322dfb95b106bd4d811f2
Sekwah <contact@sekwah.com>
2022-07-12 03:24:48 +0100

    fix: update class names to the latest forge renames

--- src/main/java/com/sekwah/narutomod/datagen/NarutoDataGenerators.java
+++ src/main/java/com/sekwah/narutomod/datagen/NarutoDataGenerators.java
@@ -1,9 +1,9 @@
 package com.sekwah.narutomod.datagen;
 
 import net.minecraft.data.DataGenerator;
+import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class NarutoDataGenerators {
