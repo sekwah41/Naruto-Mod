301863b2c2fdca38091a1c363848dfa19f751d36
Sekwah <contact@sekwah.com>
2022-09-18 22:14:12 +0100

    feat: chakra charging animation

--- src/main/java/com/sekwah/narutomod/anims/PlayerAnimHandler.java
+++ src/main/java/com/sekwah/narutomod/anims/PlayerAnimHandler.java
@@ -1,5 +1,7 @@
 package com.sekwah.narutomod.anims;
 
+import com.sekwah.narutomod.abilities.NarutoAbilities;
+import com.sekwah.narutomod.capabilities.NinjaCapabilityHandler;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -7,6 +9,13 @@ import net.minecraft.world.entity.LivingEntity;
 public class PlayerAnimHandler {
 
     public static <T extends LivingEntity> void sprintingAnim(Entity entity, PlayerModel playerModel) {
+        entity.getCapability(NinjaCapabilityHandler.NINJA_DATA).ifPresent((ninjaData -> {
+            var channeledAbility = ninjaData.getCurrentlyChanneledAbility();
+            if (channeledAbility != null && channeledAbility.equals(NarutoAbilities.CHAKRA_CHARGE.getId())) {
+                playerModel.rightArm.setRotation(-1.375616F, -0.5948606F, 0F);
+                playerModel.leftArm.setRotation(-1.375616F, 0.5948606F, 0F);
+            }
+        }));
         if(entity.isSprinting() && !entity.isVisuallySwimming()) {
             playerModel.rightArm.setRotation(1.412787F, 0F, 0F);
             playerModel.rightArm.setPos(-5F, 3.933333F, -3F - 2F);
