77d74bcf01480d875dfee34a90c428476fd7c1f8
Sekwah <contact@sekwah.com>
2022-09-18 21:38:31 +0100

    refactor: remove unneeded ninja running flags

--- src/main/java/com/sekwah/narutomod/accessors/NinjaPlayerModelAccessor.java
+++
@@ -1,6 +0,0 @@
-package com.sekwah.narutomod.accessors;
-
-public interface NinjaPlayerModelAccessor {
-    boolean isNinjaRunning();
-    void setNinjaRunning(boolean running);
-}
--- src/main/java/com/sekwah/narutomod/mixin/client/PlayerModelMixin.java
+++ src/main/java/com/sekwah/narutomod/mixin/client/PlayerModelMixin.java
@@ -1,6 +1,5 @@
 package com.sekwah.narutomod.mixin.client;
 
-import com.sekwah.narutomod.accessors.NinjaPlayerModelAccessor;
 import com.sekwah.narutomod.anims.PlayerAnimHandler;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.PlayerModel;
@@ -14,9 +13,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerModel.class)
-public class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> implements NinjaPlayerModelAccessor {
-
-    private boolean isNinjaRunning = false;
+public class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> {
 
     public PlayerModelMixin(ModelPart p_170677_) {
         super(p_170677_, RenderType::entityTranslucent);
@@ -25,15 +22,5 @@ public class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> i
     @Inject(method = "setupAnim(Lnet/minecraft/world/entity/Entity;FFFFF)V", at = @At(value = "TAIL"))
     public void setupAnim(Entity player, float par2, float par3, float par4, float par5, float par6, CallbackInfo ci) {
         PlayerAnimHandler.sprintingAnim(player, (PlayerModel) (Object) this);
-    }
-
-    @Override
-    public boolean isNinjaRunning() {
-        return this.isNinjaRunning;
-    }
-
-    @Override
-    public void setNinjaRunning(boolean running) {
-        this.isNinjaRunning = running;
     }
 }
