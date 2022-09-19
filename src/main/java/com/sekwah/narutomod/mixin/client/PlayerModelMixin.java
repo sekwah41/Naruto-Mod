552a8ee66ab358d577d8705691c5ce525f6557b8
Sekwah <contact@sekwah.com>
2022-09-19 02:26:37 +0100

    fix: first person arm with new sprinting anim

--- src/main/java/com/sekwah/narutomod/mixin/client/PlayerModelMixin.java
+++ src/main/java/com/sekwah/narutomod/mixin/client/PlayerModelMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,7 +19,11 @@ public class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> {
     }
 
     @Inject(method = "setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/client/model/HumanoidModel;setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V"))
-    public void setupAnim(T player, float par2, float par3, float par4, float par5, float par6, CallbackInfo ci) {
+    public void setupAnim(T player, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
+        if(limbSwing == 0 && ageInTicks == 0 && netHeadYaw == 0 && headPitch == 0) {
+            // Likely is firstpersonrender but try to find a better way of doing this.
+            return;
+        }
         PlayerAnimHandler.sprintingAnim(player, (PlayerModel) (Object) this);
         this.hat.copyFrom(this.head);
     }
